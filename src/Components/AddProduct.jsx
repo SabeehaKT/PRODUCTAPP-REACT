99cbe364a01a6ea76c7b9035e485539af9255598
SabeehaKT <kampalasabeeha@gmail.com>
2024-06-11 08:48:13 +0530

    product app with backend

---
+++ src/Components/AddProduct.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import NavBar from './NavBar'
+
+const AddProduct = () => {
+    const [data,setData]=useState(
+        {
+            "proid":"",
+            "proname":"",
+            "category":"",
+            "price":""
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
+  return (
+    <div>
+        <NavBar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Product ID</label>
+                            <input type="text" className="form-control" name="proid" value={data.proid} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Product Name</label>
+                            <input type="text" className="form-control" name="proname" value={data.proname} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Category</label>
+                            <select name="category" id="" className="form-control" value={data.category} onChange={inputHandler}>
+                                <option value="Garments">Garments</option>
+                                <option value="Electronic Gadgets">Electronic Gadgets</option>
+                                <option value="Kitchen Utensils">Kitchen Utensils</option>
+                                <option value="Stationary">Stationary</option>
+                            </select>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Price</label>
+                            <input type="text" className="form-control" name="price" value={data.price} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success" onClick={readValue}>Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddProduct
---
+++ src/Components/ViewAll.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import NavBar from './NavBar'
+
+const ViewAll = () => {
+    const [data,setData]=useState(
+        [
+            {
+                "proid":1001,
+                "proname":"iPhone",
+                "category":"Gadgets",
+                "price":155000,
+            }
+        ]
+
+    )
+    return (
+        <div>
+            <NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">PRODUCT ID</th>
+                                            <th scope="col">PRODUCT NAME</th>
+                                            <th scope="col">CATEGORY</th>
+                                            <th scope="col">PRICE</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {data.map(
+                                        (value,indes)=>{
+                                            return <tr>
+                                            <th scope="row">{value.proid}</th>
+                                            <td>{value.proname}</td>
+                                            <td>{value.category}</td>
+                                            <td>{value.price}</td>
+                                        </tr>
+                                        }
+                                        )}
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
